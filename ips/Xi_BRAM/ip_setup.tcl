9e3ee1abd81817ddb305b663ef773cc301e16289
sudarsha <sudarsha@rhrk.uni-kl.de>
2021-02-02 12:29:34 +0100

    XDC file upload and IP generation script update

--- ips/Xi_BRAM/ip_setup.tcl
+++ ips/Xi_BRAM/ip_setup.tcl
@@ -6,14 +6,14 @@
 ###############################################################################
 
 #Full Path of the location where TU-KL memory controller is cloned - Should be configured
-set MEMCTRL_DIR C:/MyFiles/chirag/memctrl
+set MEMCTRL_DIR C:/MyFiles/chirag/memctrl/Open_source_FPGA/DDR4_controller
 
 #Do not disturb
-set IP_DIR $MEMCTRL_DIR/dram.ctrl.verilog/ips/Xi_BRAM
+set IP_DIR $MEMCTRL_DIR/ips/Xi_BRAM
 
 #Set the location for Xilinx specific Modelsim compiled lib - Should be configured
 #set SIM_LIB_PATH "C:/Users/chirag/project_3/project_1.cache/compile_simlib/modelsim"
-set SIM_LIB_PATH "$MEMCTRL_DIR/dram.ctrl.verilog/validation"
+set SIM_LIB_PATH "$MEMCTRL_DIR/validation"
 
 #Set the Modelsim executatble folder path
 set MODELSIM_EXE_PATH "C:/modeltech64_2019.1/win64"
--- ips/Xi_Phy/ip_setup.tcl
+++ ips/Xi_Phy/ip_setup.tcl
@@ -6,19 +6,19 @@
 ###############################################################################
 
 #Full Path of the location where TU-KL memory controller is cloned - Should be configured
-set MEMCTRL_DIR C:/MyFiles/chirag/memctrl
+set MEMCTRL_DIR C:/MyFiles/chirag/memctrl/Open_source_FPGA/DDR4_controller
 
 #Do not disturb
-set IP_DIR $MEMCTRL_DIR/dram.ctrl.verilog/ips/Xi_Phy
-set IP_PATH $MEMCTRL_DIR/dram.ctrl.verilog/ips/Xi_Phy/IP/mig_phy
-set SYNTH_RES_PATH $MEMCTRL_DIR/dram.ctrl.verilog/runs/FPGA/SYNTH
-set IMPL_RES_PATH $MEMCTRL_DIR/dram.ctrl.verilog/runs/FPGA/IMPL
-set BIT_RES_PATH $MEMCTRL_DIR/dram.ctrl.verilog/runs/FPGA/BIT
-set CONST_PATH $MEMCTRL_DIR/dram.ctrl.verilog/runs/FPGA/CONST
+set IP_DIR $MEMCTRL_DIR/ips/Xi_Phy
+set IP_PATH $MEMCTRL_DIR/ips/Xi_Phy/IP/mig_phy
+set SYNTH_RES_PATH $MEMCTRL_DIR/runs/FPGA/SYNTH
+set IMPL_RES_PATH $MEMCTRL_DIR/runs/FPGA/IMPL
+set BIT_RES_PATH $MEMCTRL_DIR/runs/FPGA/BIT
+set CONST_PATH $MEMCTRL_DIR/runs/FPGA/CONST
 
 #Set the location for Xilinx specific Modelsim compiled lib - Should be configured
 #set SIM_LIB_PATH "C:/Users/chirag/project_3/project_1.cache/compile_simlib/modelsim"
-set SIM_LIB_PATH "$MEMCTRL_DIR/dram.ctrl.verilog/validation"
+set SIM_LIB_PATH "$MEMCTRL_DIR/validation"
 
 #Set the Modelsim executatble folder path
 set MODELSIM_EXE_PATH "C:/modeltech64_2019.1/win64"
